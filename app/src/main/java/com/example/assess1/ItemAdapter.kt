143077b7206084052f14214162c6eb59c9496a15
ratanjot8 <ratanjot96@gmail.com>
2020-10-06 12:42:26 +0530

    1st commit

--- app/src/main/java/com/example/assess1/ItemAdapter.kt
+++ app/src/main/java/com/example/assess1/ItemAdapter.kt
@@ -1,4 +1,41 @@
 package com.example.assess1
 
-class ItemAdapter {
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.text_item_view.view.*
+
+class ItemAdapter (val context: Context, val items:List<Item>):RecyclerView.Adapter<ItemAdapter.MyViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.MyViewHolder {
+        val view=LayoutInflater.from(context).inflate(R.layout.text_item_view,parent,false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemAdapter.MyViewHolder, position: Int) {
+        val item=items[position]
+        holder.setData(item,position)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class MyViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+        init{
+            itemView.setOnClickListener{
+                val intent=Intent(this,Description::class.java)
+                val bundle:Bundle=intent.extras
+            }
+        }
+
+        fun setData(item:Item?,pos:Int){
+             itemView.tv.text=item!!.name
+         }
+    }
 }
--- app/src/main/java/com/example/assess1/MainActivity.kt
+++ app/src/main/java/com/example/assess1/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.assess1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        val layoutManager=LinearLayoutManager(this)
+        layoutManager.orientation=LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager=layoutManager
+
+        val adapter=ItemAdapter(this,Supplier.items)
+        recyclerView.adapter=adapter
     }
 }
